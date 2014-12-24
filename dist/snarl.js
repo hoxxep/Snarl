824d544354b1abdbf5667da0a83af5819664f8eb
Liam Gray <hoxxep@gmail.com>
2014-12-24 20:27:20 +0000

    Refactored to use .editNotification(id, options)

--- dist/snarl.js
+++ dist/snarl.js
@@ -25,6 +25,25 @@
 
     var Snarl = Snarl || {};
 
+    /** Private functions */
+    function addNotificationHTML(id) {
+        if (Snarl.notifications[id] === undefined) {
+            Snarl.notifications[id] = {};
+        }
+        if (Snarl.notifications[id].element === null || Snarl.notifications[id].element === undefined) {
+            var notificationContent = '<h3 class="title"></h3><p class="text"></p>',
+                notificationWrapper = document.createElement('div');
+            notificationWrapper.innerHTML = notificationContent;
+            notificationWrapper.className = 'snarl-notification';
+            notificationWrapper.setAttribute('id', 'snarl-notification-' + id);
+            Snarl.notifications[id].element = notificationWrapper;
+        }
+        if (Snarl.notifications[id].element.parentElement === null) {
+            document.getElementById('snarl-wrapper').appendChild(Snarl.notifications[id].element);
+        }
+    }
+
+    /** Public object */
     Snarl = {
         count: 0,
         notifications: {},
@@ -43,41 +62,48 @@
         },
 
         addNotification: function(options) {
-            var notificationText = '<h3 class="title">' + options.title + '</h3><p class="text">' + options.text + '</p>',
-                notificationWrapper = document.createElement('div'),
-                notification = {};
-
-            notificationWrapper.className = 'snarl-notification';
-            notificationWrapper.innerHTML = notificationText;
-
             Snarl.count += 1;
             var id = Snarl.makeID();
             while (Snarl.notifications[id] !== undefined) {
                 id = Snarl.makeID();
             }
-            notificationWrapper.setAttribute('id', 'snarl-notification-' + id);
 
-            document.getElementById('snarl-wrapper').appendChild(notificationWrapper);
+            //TODO: merge default options
 
-            var timer = null;
-            if (options.timeout === undefined) {
-                options.timeout = 5000;
-            }
-            if (options.timeout !== null) {
-                timer = setTimeout(function() {
-                    //FUTURE: remove item from dictionary
-                    //document.getElementById('snarl-wrapper').removeChild(notificationWrapper);
-                    Snarl.removeNotification(id);
-                }, options.timeout);
-            }
-
-            notification.timeout = options.timeout;
-            notification.action = options.action;
-            notification.timer = timer;
-            notification.active = true;
-            Snarl.notifications[id] = notification;
+            addNotificationHTML(id);
+            Snarl.editNotification(id, options);
 
             return id;  // allow 3rd party code to manipulate notification
+        },
+
+        editNotification: function(id, options) {
+            addNotificationHTML(id);
+
+            //manage options
+            var element = Snarl.notifications[id].element;
+            if (options.text !== undefined) {
+                element.getElementsByClassName('text')[0].textContent = options.text;
+            }
+            if (options.title !== undefined) {
+                element.getElementsByClassName('title')[0].textContent = options.title;
+            }
+            if (options.timeout !== undefined) {
+                var timer = null;
+                if (options.timeout === undefined) {
+                    options.timeout = 5000;
+                }
+                if (options.timeout !== null) {
+                    timer = setTimeout(function() {
+                        //FUTURE: remove item from dictionary?
+                        Snarl.removeNotification(id);
+                    }, options.timeout);
+                }
+                Snarl.notifications[id].timer = timer;
+                Snarl.notifications[id].timeout = options.timeout;
+            }
+            if (options.action !== undefined) {
+                Snarl.notifications[id].action = options.action;
+            }
         },
 
         removeNotification: function(id) {
@@ -118,34 +144,19 @@
         },
 
         isDismissed: function(id) {
-            return Snarl.notifications[id].active;
+            return Snarl.notifications[id].element.parentElement !== null;
         },
 
         setTitle: function(id, title) {
-            document.getElementById('snarl-notification-' + id).getElementsByClassName('title')[0].textContent = title;
+            Snarl.editNotification(id, {title: title});
         },
 
         setText: function(id, text) {
-            document.getElementById('snarl-notification-' + id).getElementsByClassName('text')[0].textContent = text;
+            Snarl.editNotification(id, {text: text});
         },
 
         setTimeout: function(id, timeout) {
-            var timer = Snarl.notifications[id].timer;
-            if (timer !== null) {
-                clearTimeout(timer);
-            }
-            timer = null;
-            if (timeout !== null) {
-                timer = setTimeout(function() {
-                    Snarl.removeNotification(id);
-                }, timeout);
-            }
-            Snarl.notifications[id].timer = timer;
-            Snarl.notifications[id].timeout = timeout;
-
-            if (!Snarl.notifications[id].active) {
-                //TODO: if is dismissed re-show notification
-            }
+            Snarl.editNotification(id, {timeout: timeout});
         }
     };
 
