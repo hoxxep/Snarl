36870c0206a1639968336b6a03173ece8d5a1302
Liam Gray <hoxxep@gmail.com>
2014-12-26 21:53:47 +0000

    Added .exists() etc to make Snarl a little cleaner

--- dist/snarl.js
+++ dist/snarl.js
@@ -12,43 +12,19 @@
 
     */
 
+    //TODO: add success, error, warning, and custom icons?
     //TODO: add errors when id not found (IndexError?)
     //TODO: add not dismissable option
-    //TODO: merge addNotification and reopenNotification?
-    // eg. when id supplied then attempt to reopen it?
+    //TODO: merge addNotification and reopenNotification into
+    // a single openNotification(id, options) class?
+    // eg. if no notification exists for id then create a new
+    //     one and return new id otherwise reopen existing?
+
     //FUTURE: optional sidebar with past/missed notifications
 
     var Snarl = Snarl || {};
 
     var snarlCloseSVG = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve" height="100px" width="100px"><g><path d="M49.5,5c-24.9,0-45,20.1-45,45s20.1,45,45,45s45-20.1,45-45S74.4,5,49.5,5z M71.3,65.2c0.3,0.3,0.5,0.7,0.5,1.1   s-0.2,0.8-0.5,1.1L67,71.8c-0.3,0.3-0.7,0.5-1.1,0.5s-0.8-0.2-1.1-0.5L49.5,56.6L34.4,71.8c-0.3,0.3-0.7,0.5-1.1,0.5   c-0.4,0-0.8-0.2-1.1-0.5l-4.3-4.4c-0.3-0.3-0.5-0.7-0.5-1.1c0-0.4,0.2-0.8,0.5-1.1L43,49.9L27.8,34.9c-0.6-0.6-0.6-1.6,0-2.3   l4.3-4.4c0.3-0.3,0.7-0.5,1.1-0.5c0.4,0,0.8,0.2,1.1,0.5l15.2,15l15.2-15c0.3-0.3,0.7-0.5,1.1-0.5s0.8,0.2,1.1,0.5l4.3,4.4   c0.6,0.6,0.6,1.6,0,2.3L56.1,49.9L71.3,65.2z"/></g></svg>';
-
-    /** Private functions */
-    function addNotificationHTML(id) {
-        if (Snarl.notifications[id] === undefined) {
-            Snarl.notifications[id] = {};
-        }
-        if (Snarl.notifications[id].element === null || Snarl.notifications[id].element === undefined) {
-            var notificationContent = '<h3 class="title"></h3><p class="text"></p><div class="snarl-close"><!--<i class="fa fa-close"></i>-->' + snarlCloseSVG + '</div>',
-                notificationWrapper = document.createElement('div');
-            notificationWrapper.innerHTML = notificationContent;
-            notificationWrapper.className = 'snarl-notification';
-            notificationWrapper.setAttribute('id', 'snarl-notification-' + id);
-            Snarl.notifications[id].element = notificationWrapper;
-        }
-        if (Snarl.notifications[id].element.parentElement === null) {
-            document.getElementById('snarl-wrapper').appendChild(Snarl.notifications[id].element);
-        }
-    }
-    
-    /**
-     * Overwrites obj1's values with obj2's and adds obj2's if non existent in obj1
-     */
-    function mergeOptions(defaults, options) {
-        var merged = {}, attrname;
-        for (attrname in defaults) {merged[attrname] = defaults[attrname];}
-        for (attrname in options) {merged[attrname] = options[attrname];}
-        return merged;
-    }
 
     /** Public object */
     Snarl = {
@@ -65,26 +41,9 @@
             Snarl.defaultOptions = mergeOptions(Snarl.defaultOptions, options);
         },
 
-        makeID: function() {
-            var id = '';
-            var possible = 'abcdefghijklmnopqrstuvwxyz0123456789';
-
-            // check ID doesn't already exist and regen if needed
-            do {
-                id = '';
-                for(var i=0; i<5; i++) {
-                    id += possible.charAt(
-                        Math.floor(Math.random() * possible.length)
-                    );
-                }
-            } while (Snarl.notifications[id] !== undefined);
-
-            return id;
-        },
-
         addNotification: function(options) {
             Snarl.count += 1;
-            var id = Snarl.makeID();
+            var id = makeID();
 
             addNotificationHTML(id);
             Snarl.editNotification(id, options);
@@ -145,47 +104,16 @@
             }
         },
 
-        clickNotification: function(event) {
-            if (event.toElement.getAttribute('id') === 'snarl-wrapper') {
-                return;
-            }
-
-            var maxDepth = 5,
-                notification = event.toElement,
-                close = false;
-            while (notification.className.lastIndexOf('snarl-notification') === -1) {
-                if (maxDepth > 0) {
-                    if (notification.className.lastIndexOf('snarl-close') !== -1) {
-                        close = true;
-                    }
-                    notification = notification.parentElement;
-                } else {
-                    console.debug('Clicked inside #snarl-wrapper but no notification was found?');
-                    return;
-                }
-            }
-
-            var id = notification.getAttribute('id');
-            id = /snarl-notification-([a-zA-Z0-9]+)/.exec(id)[1];
-
-            if (close) {
-                Snarl.removeNotification(id);
+        isDismissed: function(id) {
+            if (Snarl.exists(id)) {
+                return Snarl.notifications[id].element.parentElement === null;
             } else {
-                var action = Snarl.notifications[id].action;
-                //console.log('clicking: ' + close + ' ' + action);
-                if (action === undefined || action === null) {
-                    return;
-                } else if (typeof action === "string") {
-                    //TODO: do this better
-                    window.location = action;
-                } else if (typeof action === "function") {
-                    action(); //TODO: add some cb info (what's clicked)
-                }
+                return true;
             }
         },
 
-        isDismissed: function(id) {
-            return Snarl.notifications[id].element.parentElement !== null;
+        exists: function(id) {
+            return Snarl.notifications[id] !== undefined;
         },
 
         setTitle: function(id, title) {
@@ -202,19 +130,115 @@
     };
 
 
-    function snarlInitialize() {
-        var snarlWrapper = document.createElement('div');
-        snarlWrapper.setAttribute('id', 'snarl-wrapper');
+    /**
+     * Generate random ID string and check it's not already
+     * been used
+     */
+    function makeID() {
+        var id = '';
+        var possible = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-        // only one event handler thanks to bubbling
-        snarlWrapper.addEventListener('click', Snarl.clickNotification);
-        document.body.appendChild(snarlWrapper);
+        do {
+            id = '';
+            for(var i=0; i<5; i++) {
+                id += possible.charAt(
+                    Math.floor(Math.random() * possible.length)
+                );
+            }
+        } while (Snarl.exists(id));
+
+        return id;
+    }
+
+
+    /**
+     * Handle all click events in notifications
+     */
+    function clickNotification(event) {
+        if (event.toElement.getAttribute('id') === 'snarl-wrapper') {
+            return;
+        }
+
+        var maxDepth = 5,
+            notification = event.toElement,
+            close = false;
+        while (notification.className.lastIndexOf('snarl-notification') === -1) {
+            if (maxDepth > 0) {
+                if (notification.className.lastIndexOf('snarl-close') !== -1) {
+                    close = true;
+                }
+                notification = notification.parentElement;
+            } else {
+                console.debug('Clicked inside #snarl-wrapper but no notification was found?');
+                return;
+            }
+        }
+
+        var id = notification.getAttribute('id');
+        id = /snarl-notification-([a-zA-Z0-9]+)/.exec(id)[1];
+
+        if (close) {
+            Snarl.removeNotification(id);
+        } else {
+            var action = Snarl.notifications[id].action;
+            //console.log('clicking: ' + close + ' ' + action);
+            if (action === undefined || action === null) {
+                return;
+            } else if (typeof action === "string") {
+                //TODO: do this better
+                window.location = action;
+            } else if (typeof action === "function") {
+                action(); //TODO: add some cb info (what's clicked)
+            }
+        }
+    }
+
+
+    /**
+     * Injects base notification html to document if it's
+     * not been added yet.
+     */
+    function addNotificationHTML(id) {
+        if (Snarl.notifications[id] === undefined) {
+            Snarl.notifications[id] = {};
+        }
+        if (Snarl.notifications[id].element === null || Snarl.notifications[id].element === undefined) {
+            var notificationContent = '<h3 class="title"></h3><p class="text"></p><div class="snarl-close"><!--<i class="fa fa-close"></i>-->' + snarlCloseSVG + '</div>',
+                notificationWrapper = document.createElement('div');
+            notificationWrapper.innerHTML = notificationContent;
+            notificationWrapper.className = 'snarl-notification';
+            notificationWrapper.setAttribute('id', 'snarl-notification-' + id);
+            Snarl.notifications[id].element = notificationWrapper;
+        }
+        if (Snarl.notifications[id].element.parentElement === null) {
+            document.getElementById('snarl-wrapper').appendChild(Snarl.notifications[id].element);
+        }
+    }
+
+
+    /**
+     * Overwrites default's values with options's and adds
+     * options's if non-existent in default
+     */
+    function mergeOptions(defaults, options) {
+        var merged = {}, attrname;
+        for (attrname in defaults) {merged[attrname] = defaults[attrname];}
+        for (attrname in options) {merged[attrname] = options[attrname];}
+        return merged;
     }
 
 
     /*
      * If library is injected after page has loaded
      */
+    function snarlInitialize() {
+        var snarlWrapper = document.createElement('div');
+        snarlWrapper.setAttribute('id', 'snarl-wrapper');
+
+        // only one event handler thanks to bubbling
+        snarlWrapper.addEventListener('click', clickNotification);
+        document.body.appendChild(snarlWrapper);
+    }
 
     (function () {
         if (document.readyState === 'complete' || document.readyState === 'interactive' && document.body) {
